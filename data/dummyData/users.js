9d48b522ef08925140cd2eef9a94e03aa7fd07a9
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-05-23 19:15:30 -0400

    created dummydata for the users and 2 seeds: 1 for truncating data so the ids do not continue after refresh of the seed data and the second is the user seed date using the dummydata

---
+++ data/dummyData/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    username: "user1",
+    // password: "$2b$10$yGNKn1RJt4ODgHpJP/7GxOw2Ll41qsPa5/9es0GhIeEPcJUVRkOBi", // password
+    password: 'password1'
+  },
+  {
+    username: "user2",
+    // password: "$2b$10$6hdITF225N.1Kai6LToIBeZqdOAaMqlniGGihQ8os5aIOJRUQ.OPu", // password
+    password: 'password2'
+  },
+  {
+    username: "user3",
+    // password: "$2b$10$Vw2QvobiXwhV6SXxAjV1UemBFMR5N7ZIyAxsmVsvbxUOqxdVuaKAy", // password
+    password: 'password3'
+  },
+  {
+    username: "user4",
+    // password: "$2b$10$LfpSmsQFgeJ93va35Z3LQOc7OF4lG96/9jxniD629laQK4xrHgqRS", // password
+    password: 'password4'
+  },
+  {
+    username: "user5",
+    // password: "$2b$10$LfpSmsQFgeJ93va35Z3LQOc7OF4lG96/9jxniD629laQK4xrHgqRS", // password
+    password: 'password5'
+  }
+]
