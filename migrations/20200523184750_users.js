152084674411600b62bed84c34f2d5ad451c327f
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-05-23 18:58:12 -0400

    create users folder to hold user model and router, created migration folder to hold migrations, created users table

---
+++ migrations/20200523184750_users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table
+      .increments('user_id');
+
+    table
+      .varchar('username', 65).notNullable().unique()
+
+    table
+      .varchar('password', 65).notNullable().unique()
+
+    table
+      .timestamps(true, true)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
