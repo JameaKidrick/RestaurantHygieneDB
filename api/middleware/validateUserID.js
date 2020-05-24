8ea0fa4f0255bb51358088ebe710d1fc00d43349
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-05-23 21:10:56 -0400

    added middleware to check if user with the given id is in the database, added users router to the server.js file, created all CRUD for user data except for creation

---
+++ api/middleware/validateUserID.js
@@ -0,0 +1,20 @@
+const usersDB = require('../users/usersModel');
+
+const validateUserID = (req, res, next) => {
+  const id = req.params.userid;
+
+  usersDB.findById(id)
+    .then(user => {
+      if(!user) {
+        res.status(404).json({ error: `A user with the id ${id} does not exist in the database.` })
+      }else{
+        req.user = user;
+        next();
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Internal server error', error})
+    })
+}
+
+module.exports = validateUserID;
--- api/server.js
+++ api/server.js
@@ -3,12 +3,17 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const usersRouter = require('../api/users/usersRouter');
+
 const server = express();
+
 
 server.use(helmet());
 server.use(morgan('dev'));
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send('Hello World!')
