370314a1eed357be4c92f84d89777f32ecea395a
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-06-04 09:27:59 -0400

    added notes for googles api and added error checks for required versus optional fields

--- api/locator/locatorRouter.js
+++ api/locator/locatorRouter.js
@@ -13,18 +13,47 @@ const router = express.Router();
   // OPTIONAL PARAMETERS:
     // fields - properties desired; options: business_status, formatted_address, geometry, icon,name, permanently_closed, photos, place_id, plus_code, types
       // WITHOUT fields parameter, only the place_id will be returned
-    // locationbias - preferred specific area; can use circle:radius@lat,lng OR point:lat,lng
+    // locationbias - preferred specific area; can use circle:radius@lat,lng OR point:lat,lng 
+    // IF NO PLACE WITHIN, 2000 METERS OF SPECIFICED COORDINATES, PLACE NEAREST TO IP WILL TRY TO BE FOUND
+    // IF NO PLACE FOUND RESULTS IN CANDIDATES LENGTH === 0 AND STATUS OF 'ZERO_RESULTS'
       // WITHOUT locationbias parameter, the API will use the IP address
 router.post('/', (req, res) => {
-  axios
-    .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.body.input}&inputtype=${req.body.inputType}&fields=${req.body.fields}&locationbias=${req.body.location}&key=${process.env.GOOGLE_API_KEY}`)
-    .then(response => {
-      res.status(200).json(response.data)
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Internal server error', error })
-    })
+  const key = process.env.GOOGLE_API_KEY
+  const input = req.body.input
+  const inputType = req.body.inputType
+  const fields = req.body.fields
+  const location = req.body.location
+  let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${input}&inputtype=${inputType}`
+
+  if(fields !== ''){
+    url += `&fields=${fields}`
+  }
+  if(location !== ''){
+    url += `&locationbias=${location}`
+  }
+
+  url += `&key=${key}`
+  console.log('INPUT', location, url)
+
+  if(!input){
+    res.status(400).json({ error: 'Please include the place name, address, or phone number.' })
+  }else if(!inputType){
+    res.status(400).json({ error: 'Please define input type. Use either `textquery` or `phonenumber`.' })
+  }else if((inputType !== 'textquery') && (inputType !== 'phonenumber')){
+    res.status(400).json({ error: 'That is not a valid input type. Please use either `textquery` or `phonenumber`.'})
+  }else {
+    axios
+      .get(url)
+      .then(response => {
+        res.status(200).json(response.data)
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'Internal server error', error })
+      })
+  }
 })
+
+
 
 
 
